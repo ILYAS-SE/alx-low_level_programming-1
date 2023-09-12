9703d5a7025869540137f8724d7466be5d689de7
root <root@DESKTOP-S34USOL>
2023-09-12 13:15:11 +0100

    0. _putcha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A the programm that  print _putchar followed by a new line.
+ * Return: 0
+*/
+
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
