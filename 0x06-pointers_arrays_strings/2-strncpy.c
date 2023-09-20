c28b93aa4ae227a0e0ed94a4e7cb5a2ffae1dc68
ELMbadre <141264910+ELMbadre@users.noreply.github.com>
2023-09-20 16:11:39 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * strncp : is a C library function used for copying a specified number of characters from one string to another. It stands for "string copy with a specified maximum number of characters"
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equal,
+ * another number if not.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
-        int i;
+	int i = 0;
 
-        for (i = 0; i < n && src[i] != '\0'; i++)
-                *(dest + i) = *(src + i);
-        for ( ; i < n; i++)
-                *(dest + i) = '\0';
-        return (dest);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
