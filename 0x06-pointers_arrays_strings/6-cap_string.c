23975ea4d460778917e089b0b1d52376a2e7de29
root <root@DESKTOP-S34USOL>
2023-09-20 21:16:05 +0100

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes the first letter of each word in a string
+ *
+ * @s: Pointer to the input string
+ *
+ * Return: Pointer to the modified string
+ *
+ * Description: This function iterates through a string and capitalizes
+ * the first letter of each word if it is preceded by a space,tab,(\n),
+ * comma, semicolon, period, exclamation point, question mark,(""),
+ * opening parenthesis,closing parenthesis, opening curly brace, or closing
+ * curly brace.It then returns the modified string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+					s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+					s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+					s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+					s[i - 1] == '}')
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
