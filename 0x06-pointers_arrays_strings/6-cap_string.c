259519dae747beda76e7e8e1b8f1c6bf1ad0e534
ELMbadre <141264910+ELMbadre@users.noreply.github.com>
2023-09-20 21:31:59 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,8 @@
 #include "main.h"
 /**
- * cap_string - Capitalizes the first letter of each word in a string
- *
- * @s: Pointer to the input string
- *
- * Return: Pointer to the modified string
- *
- * Description: This function iterates through a string and capitalizes
- * the first letter of each word if it is preceded by a space,tab,(\n),
- * comma, semicolon, period, exclamation point, question mark,(""),
- * opening parenthesis,closing parenthesis, opening curly brace, or closing
- * curly brace.It then returns the modified string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 char *cap_string(char *s)
 {
@@ -20,7 +12,7 @@ char *cap_string(char *s)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+			if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
 					s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
 					s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
 					s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
