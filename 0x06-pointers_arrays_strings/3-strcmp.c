80fd221be9810b61eddb756a60d1521782225797
ELMbadre <141264910+ELMbadre@users.noreply.github.com>
2023-09-20 16:17:19 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
 /**
- * strcmp : The strcmp function you provided appears to be a custom implementation or prototype for the strcmp function.
- * @s1 : are pointers to the null-terminated strings you want to compare.
- * @s2 : are pointers to the null-terminated strings you want to compare.
- * The function returns an integer value as described in the previous response.
+ * _strcmp - Compares two strings
+ *
+ * @s1: Pointer to the first string
+ * @s2: Pointer to the second string
+ *
+ * Return:(-)value if s1 < s2, 0 if s1 == s2, (+) value if s1 > s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
+
+	while (s1[i] && s2[i] && s1[i] ==  s2[i])
 		i++;
 	return (s1[i] - s2[i]);
 }
