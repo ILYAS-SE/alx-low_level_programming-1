dde2e555dd6fe8ce9d141008dbc24ff8212117c6
ELMbadre <141264910+ELMbadre@users.noreply.github.com>
2023-09-21 12:44:59 +0100

    All tasks finished! 🏁

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,8 +9,13 @@
 
 char *string_toupper(char *str)
 {
-	while (*str++)
-		if (*str >= 'a' && *str <= 'z')
-			*str -= 32;
-	return (str);
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (j);
 }
